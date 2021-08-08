40469be00a5637a2f7a360b75ccdd097b53cef0a
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2021-08-08 17:05:39 +0800

    Added Search Feature for password manager

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
 
 EMAIL = "sheen.an.goh@gmail.com"
-
+FILE = "passwords.json"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
 
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
@@ -37,20 +39,47 @@ def save():
     password = password_entry.get()
 
     if len(website) == 0 or len(password) == 0 or len(email) == 0:
-        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+        messagebox.showinfo(
+            title="Oops", message="Please don't leave any fields empty!")
         return
 
-    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
-                                                          f"\nPassword: {password}"
-                                                          f"\nIs it ok to save? ")
-    if not is_ok:
-        return
-    with open("passwords.txt", "a") as password_file:
-        password_file.write(f"{website} | {email} | {password}\n")
+    new_password = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
+    try:
+        with open(FILE, "r") as password_file:
+            data = json.load(password_file)
+    except FileNotFoundError:
+        with open(FILE, "w") as password_file:
+            json.dump(new_password, password_file, indent=4)
+    else:
+        data.update(new_password)
+        with open(FILE, "w") as password_file:
+            json.dump(data, password_file, indent=4)
+    finally:
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+        messagebox.showinfo(
+            title="Success", message="You have successfully saved the password!")
 
-    website_entry.delete(0, END)
-    password_entry.delete(0, END)
-    messagebox.showinfo(title="Success", message="You have successfully saved the password!")
+
+def get_password():
+    website = website_entry.get()
+    try:
+        with open(FILE, "r") as password_file:
+            existing_passwords = json.load(password_file)
+            data = existing_passwords[website]
+
+            password_entry.delete(0, END)
+            password_entry.insert(0, data['password'])
+            email_entry.delete(0, END)
+            email_entry.insert(0, data['email'])
+    except (FileNotFoundError, KeyError):
+        messagebox.showinfo(
+            title="Error", message="No passwords found!")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -74,8 +103,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=39)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=20)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=39)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -84,9 +113,13 @@ password_entry = Entry(width=20)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = Button(text="Generate Password", command=generate_password)
+generate_password_button = Button(
+    text="Generate Password", command=generate_password, width=16)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=39, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+fetch_password_button = Button(text="Search", command=get_password, width=16)
+fetch_password_button.grid(row=1, column=2, columnspan=1)
+
 
 window.mainloop()
